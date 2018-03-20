5258cc6450ed6dfad99c89dd05b7530e5af95e8e
DuonVu98 <duongvutung1998@gmail.com>
2018-03-20 10:50:24 +0700

    chúng m chạy thử ver này đi

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MagicFX.fxml"));
+			AnchorPane root = loader.load();
+			Scene scene = new Scene(root, EnumSprite.WIDTH.getValue(), EnumSprite.HEIGHT.getValue());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Magic Wand");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
